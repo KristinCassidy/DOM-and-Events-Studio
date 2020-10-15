ec54d3ec902e07ad6d86b0bd7435e2089edc87b3
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-15 04:24:24 -0500

    takeoff

--- scripts.js
+++ scripts.js
@@ -1,21 +1,24 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
-window.onload;
-
-button.addEventListener("click", function() {
-    window.confirm("Confirm that the shuttle is ready for takeoff.");
-    let p = document.getElementById("flightStatus");
-    p.innerHTML = "Shuttle in flight.";
-    let shuttleScreen = document.getElementById("shuttleBackground");
-    shuttleScreen.style.background = "blue";
+let p = null;
+let shuttleScreen = null;
+function(){
+    document.getElementById("spaceShuttleHeight").innerHTML = "Window is loaded.";
+  }
+function takeoff () {
+    button.addEventListener("click", function() {
+        console.log(window.confirm("Confirm that the shuttle is ready for takeoff.");
+        p = document.getElementById("flightStatus");
+        p.innerHTML = "Shuttle in flight.";
+        let shuttleScreen = document.getElementById("shuttleBackground");
+        shuttleScreen.style.background = "blue";
+    }
 }
-
-
-
+window.onload = 
 
 //when takeoff button is clicked, the following should happen
 //  a. A window confirm should let the user know "Confirm that the shuttle is ready for takeoff." If the shuttle is ready for liftoff, then add parts b-d.
 //  b. The flight status should change to "Shuttle in flight."
 //  c. The background color of the shuttle flight screen (id = "shuttleBackground") should change from green to blue.
 //  d. The shuttle height should increase by 10,000 miles.
-
+//
