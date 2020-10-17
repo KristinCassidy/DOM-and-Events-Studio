2a7d8bdce9e5e2144f46ff8756746c56fe0fe3ad
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-17 18:30:52 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -48,9 +48,9 @@ function abortMission(event) {
                           
 function moveDown(event) {
    let height = document.getElementById("spaceShuttleHeight");
-   height.innerHTML += 1;
-   let down = document.getElementsByID("rocket");
-   down.style.top = 10 + 'px';
+   Num(height.innerHTML) += 1;
+   //let down = document.getElementsByID("rocket");
+   //down.style.top = 10 + 'px';
   
 }
 
