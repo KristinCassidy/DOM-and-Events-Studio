ecd84a6335dbbf7c53fa2f1c7dbf6071338097e1
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-16 21:34:53 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,17 +6,17 @@ function () {
     document.getElementById("spaceShuttleHeight").innerHTML = "Window is loaded.";
 };
 
-function takeoff () {
-    button.addEventListener("click", function() {
-        console.log(window.confirm("Confirm that the shuttle is ready for takeoff."));
-        p = document.getElementById("flightStatus");
-        p.innerHTML = "Shuttle in flight.";
-        shuttleScreen = document.getElementById("shuttleBackground");
-        shuttleScreen.style.background = "blue";
-    }
+
+button.addEventListener("click", function takeoff() {
+    console.log(window.confirm("Confirm that the shuttle is ready for takeoff."));
+    p = document.getElementById("flightStatus");
+    p.innerHTML = "Shuttle in flight.";
+    shuttleScreen = document.getElementById("shuttleBackground");
+    shuttleScreen.style.background = "blue";
 }
 
-window.onload = "takeoff";
+                        
+window.onload 
 
 //when takeoff button is clicked, the following should happen
 //  a. A window confirm should let the user know "Confirm that the shuttle is ready for takeoff." If the shuttle is ready for liftoff, then add parts b-d.
