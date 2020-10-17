d8114fb17ba4ef8175a453902d944ca52e1bb67f
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-17 17:44:48 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -46,7 +46,7 @@ function moveDown(event) {
    let height = document.getElementById("spaceShuttleHeight");
    height.innerHTML = 1;
    let down = document.getElementsByID("rocket");
-   down.css("top","-=10");
+   down.style.bottom = 30px;
   // img.style.padding-top += 2px;
 }
 
