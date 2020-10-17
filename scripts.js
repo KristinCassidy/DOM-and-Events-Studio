ccaf919bb3dfee04ff26d7217079449da232c825
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-16 23:19:44 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,28 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
 
-let height = document.getElementById("spaceShuttleHeight");
-let shuttleScreen = document.getElementById("shuttleBackground");
-let p = document.getElementById("flightStatus");
+//let height = document.getElementById("spaceShuttleHeight");
+//let shuttleScreen = document.getElementById("shuttleBackground");
+//let p = document.getElementById("flightStatus");
 
 function takeoff(event) {
    window.confirm("Confirm that the shuttle is ready for takeoff.");
+   let p = document.getElementById("flightStatus");
    p.innerHTML = "Shuttle in flight.";
+   let shuttleScreen = document.getElementById("shuttleBackground");
    shuttleScreen.style.background = "blue";
+   let height = document.getElementById("spaceShuttleHeight");
    height.innerHTML = 10000;
 }
 
 function landing(event) {
    window.alert("The shuttle is landing. Landing gear engaged."); 
+   let p = document.getElementById("flightStatus");
    p.innerHTML = "The shuttle has landed.";
-   //shuttleScreen.style.background = "green";
-  // height.innerHTML = 0;
+   let shuttleScreen = document.getElementById("shuttleBackground");
+   shuttleScreen.style.background = "green";
+   let height = document.getElementById("spaceShuttleHeight");
+   height.innerHTML = 0;
 }
 
                         
