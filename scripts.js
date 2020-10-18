462b018258da3ef5087c4bfe5e0dbe770ecc0cd3
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-17 20:04:55 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -52,7 +52,7 @@ function abortMission(event) {
 function moveRight(event) {
    let height = document.getElementById("spaceShuttleHeight");
    height.innerHTML = Number(height.innerHTML);
-   height.innerHTML += 1;
+   height.innerHTML += Number(1);
 }
 
 // function moveLeft(event) {
