ae8115dc118bf13dfeca3120247b5d517ca36815
KristinCassidy <72127898+KristinCassidy@users.noreply.github.com>
2020-10-18 22:14:41 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -55,7 +55,7 @@ function moveRight(event) {
    let miles = Number(height.innerHTML);
    let move = Number(miles +=1);
    height.innerHTML = move;
-   document.getElementById("rocket").style.objectPosition = "0 0 0 10px";
+   document.getElementById("rocket").style.objectPosition = "10px";
 }
 
 function moveLeft(event) {
